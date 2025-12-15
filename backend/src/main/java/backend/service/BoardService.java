50cdb54ea0c8e93d196a67fed6e54d365f0e7639
Ape07Park <ape072399@gmail.com>
2025-12-15 22:20:47 +0900

    Controller 생성

--- backend/src/main/java/backend/service/BoardService.java
+++ backend/src/main/java/backend/service/BoardService.java
@@ -2,10 +2,15 @@ package backend.service;
 
 import org.springframework.stereotype.Service;
 
+import backend.domain.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BoardService {
+
+	private final BoardRepository boardRepository;
 
 }
---
+++ backend/src/main/java/backend/web/BoardController.java
@@ -0,0 +1,16 @@
+package backend.web;
+
+import org.springframework.stereotype.Controller;
+
+import backend.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class BoardController {
+
+	private final BoardService boardService;
+
+}
